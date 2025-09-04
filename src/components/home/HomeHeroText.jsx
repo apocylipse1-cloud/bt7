342de38c2c5a6aba58853bba646e2123dcbc35b2
apocylipse1-cloud <apocylipse1@gmail.com>
2025-09-04 13:28:10 +0530

    Updated index.css

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -4,12 +4,12 @@ const HomeHeroText = () => {
   return (
     <div className="font-[font1] text-center relative depth-4 px-4 flex-1 flex items-center justify-center">
       <div className="w-full">
-        <div className="text-[12vw] sm:text-[9vw] lg:text-[9.5vw] justify-center flex items-center uppercase leading-[10vw] sm:leading-[7.5vw] lg:leading-[8vw] text-layer-3 mb-2 sm:mb-0">
+        <div className="text-[12vw] sm:text-[9vw] lg:text-[7vw] xl:text-[6vw] justify-center flex items-center uppercase leading-[10vw] sm:leading-[7.5vw] lg:leading-[6vw] xl:leading-[5vw] text-layer-3 mb-2 sm:mb-0">
           You do the work
         </div>
-        <div className="text-[12vw] sm:text-[9vw] lg:text-[9.5vw] justify-center flex items-center uppercase leading-[10vw] sm:leading-[7.5vw] lg:leading-[8vw] text-layer-3 flex-wrap justify-center mb-2 sm:mb-0">
+        <div className="text-[12vw] sm:text-[9vw] lg:text-[7vw] xl:text-[6vw] justify-center flex items-center uppercase leading-[10vw] sm:leading-[7.5vw] lg:leading-[6vw] xl:leading-[5vw] text-layer-3 flex-wrap justify-center mb-2 sm:mb-0">
           <span>we</span>
-          <div className="h-[8vw] w-[20vw] sm:h-[7vw] sm:w-[16vw] rounded-full overflow-hidden mx-2 sm:mx-2 glass glow-accent flex-shrink-0 my-1 sm:my-0">
+          <div className="h-[8vw] w-[20vw] sm:h-[7vw] sm:w-[16vw] lg:h-[5vw] lg:w-[12vw] xl:h-[4vw] xl:w-[10vw] rounded-full overflow-hidden mx-2 sm:mx-2 glass glow-accent flex-shrink-0 my-1 sm:my-0">
             <video
               className="h-full w-full object-cover"
               autoPlay
@@ -23,7 +23,7 @@ const HomeHeroText = () => {
           </div>
           <span>do the</span>
         </div>
-        <div className="text-[12vw] sm:text-[9vw] lg:text-[9.5vw] justify-center flex items-center uppercase leading-[10vw] sm:leading-[7.5vw] lg:leading-[8vw] text-layer-3">
+        <div className="text-[12vw] sm:text-[9vw] lg:text-[7vw] xl:text-[6vw] justify-center flex items-center uppercase leading-[10vw] sm:leading-[7.5vw] lg:leading-[6vw] xl:leading-[5vw] text-layer-3">
           stitches
         </div>
       </div>
